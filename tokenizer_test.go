cd47de02c2d55613b8ba538ef22f3db0237de2e5
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-02-03 17:08:20 +0900

    Add NGramTokenizer benchmark

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -7,7 +7,13 @@ import (
 func BenchmarkWhiteSpaceTokenizer(b *testing.B) {
 	t := NewWhiteSpaceTokenizer()
 	for i := 0; i < b.N; i++ {
-		t.Tokenize("Et støkke tekst å splitte opp i biter, versågod!")
+		t.Tokenize("Et støkke \"tekst\" å splitte opp i biter, versågod!")
+	}
+}
+func BenchmarkNGramTokenizer(b *testing.B) {
+	t := NewNGramTokenizer(2, 3)
+	for i := 0; i < b.N; i++ {
+		t.Tokenize("Et støkke \"tekst\" å splitte opp i biter, versågod!")
 	}
 }
 
